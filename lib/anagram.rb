3604bbe5afa5af0577a573be28038466da0139ae
Rachel Katz <rachel.katz@flatironschool.com>
2022-05-16 13:36:51 -0400

    commit tree ef38bc0b79e41144e4a73488375fd2a4721a4b0b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+    @string
+    def initialize(string)
+        @string = string
+    end 
+
+    def match(array)
+        puts @string
+        match_array = []
+        array.each do |word|
+            #binding.pry
+            puts "#{word}, #{@string.split("").sort}, #{word.split("").sort} "
+            if @string.split('').sort == word.split('').sort 
+                match_array << word 
+            end 
+        end 
+        match_array
+    end 
+end 
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
